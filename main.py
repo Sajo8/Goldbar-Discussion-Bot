bebcb1ef5998befdf7c3d3aca81a9a2ba53a2595
Andrew Splin <andrewsplin@protonmail.com>
2020-12-28 14:51:08 -0700

    remove logging

--- main.py
+++ main.py
@@ -10,16 +10,8 @@ from discord.channel import TextChannel
 from discord.message import Message
 from discord.ext import commands
 from discord import RawReactionActionEvent as RawReaction
-import logging
 import os
 import asyncio
-
-# set up logging
-logger = logging.getLogger('discord')
-logger.setLevel(logging.DEBUG)
-handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
-handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
-logger.addHandler(handler)
 
 # set up environment tokens
 try:
